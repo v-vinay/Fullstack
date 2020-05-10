8022b9e1f43c7eca18cf94eb19d7da76d1b9da29
v-vinay <v.vinayvarma1@gmail.com>
2020-05-10 17:03:14 +0530

    Grunt done

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,9 @@
 module.exports = function(grunt) {
 
     require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
-    const sass = require('node-sass');
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+      });    const sass = require('node-sass');
 
 
     grunt.initConfig({
@@ -79,10 +80,91 @@ module.exports = function(grunt) {
                     dest: 'dist/'                  // Destination path prefix
                 }]
             }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html', 'aboutus.html', 'index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function(context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase:false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {}
+        },
+        uglify: {
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            release: {
+                files: [{
+                    src: [
+                        'dist/js/*js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            }
+        },
+        usemin: {
+            html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/inde.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css', 'dist/js']
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true,
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html',
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html': 'dist/aboutus.html'
+                }
+            }
         }
 
     });
     grunt.registerTask('css',['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
-    grunt.registerTask('build', ['clean', 'copy', 'imagemin']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
 }
